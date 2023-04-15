da694ac25bc9343a5d3f966ece98362328c23df6
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-04-15 22:23:25 +0800

    Update uniformly_accelerated_motion.R

--- models/uniformly_accelerated_motion.R
+++ models/uniformly_accelerated_motion.R
@@ -4,20 +4,20 @@ v1 <- n_athelete_0
 total_t <- splits |> 
   pull(`100`) |> 
   max()
-for (i in 0:(total_t / 0.001)) {
-  time <- c(time, rep(0.001 * i, n_athelete))
-  i_over_1000 <- i / 1000
+for (i in 0:(total_t / state_t)) {
+  time <- c(time, rep(state_t * i, n_athelete))
+  i_over_1000 <- i / (1/state_t)
   if (i > min_ms) {
     for (j in 1:n_athelete) {
       if (athlete[j] %in% finished_athlete) next
       if (rt[j] <= i_over_1000) {
-        s[j] <- s[j] + v0[j] * 0.001 + 1 / 2 * a[j] * 0.001^2
+        s[j] <- s[j] + v0[j] * state_t + 1 / 2 * a[j] * state_t^2
         if (s[j] > 100) {
           s[j] <- 100
           finished_athlete <- c(finished_athlete, athlete[j])
           next
         }
-        v1[j] <- v0[j] + a[j] * 0.001
+        v1[j] <- v0[j] + a[j] * state_t
         v0[j] <- v1[j]
       }
       split_current <- s %/% 10 # check which split the athlete is in
@@ -34,5 +34,5 @@ for (i in 0:(total_t / 0.001)) {
   }
 }
 athlete_distance <- athlete |> 
-  rep(total_t / 0.001 + 1)
+  rep(total_t / state_t + 1)
 distance <- tibble(time = time, athlete = athlete_distance, distance = distance)
