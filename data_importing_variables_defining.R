af37e11813ed52bf2c9b87ec7f4adc52b6b6ef44
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-06-13 07:27:43 +0800

    Update data_importing_variables_defining.R

--- data_importing_variables_defining.R
+++ data_importing_variables_defining.R
@@ -10,10 +10,15 @@ rt <- splits |>
 min_rt <- min(rt)
 max_rt <- max(rt)
 state_t <- 0.001
-min_ms <- min_rt / state_t # state_t stands for the time between states. The animation is interpolating and fitting by states of the bars in the bar chart, and the state_t is default to 0.001 
-# second, the minimum precision stardardized by World Athlete. still, you can change this number as you wish to get a different effect or to save rendering time. 
-distance <- c(rep(0, n_athelete * min_ms), 0) # plus 1 to min_ms since 0 is also 
-# considered
+
+# state_t is the time between states
+# the animation is interpolating and fitting by states of the bars in the bar chart
+# state_t is default to 0.001 second, the minimum precision stardardized by World Athlete
+# still, you can change this number as you wish to get a different effect or to save rendering time. 
+min_ms <- min_rt / state_t  
+
+# plus 1 to min_ms since 0 is also considered
+distance <- c(rep(0, n_athelete * min_ms), 0) 
 time <- NULL
 s <- n_athelete_0
 t <- n_athelete_0
