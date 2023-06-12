9fb009124356c37fcf8c2862947370fa9936af5f
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-06-13 07:24:10 +0800

    Update uniformly_accelerated_motion.R

--- models/uniformly_accelerated_motion.R
+++ models/uniformly_accelerated_motion.R
@@ -20,10 +20,14 @@ for (i in 0:(total_t / state_t)) {
         v1[j] <- v0[j] + a[j] * state_t
         v0[j] <- v1[j]
       }
-      split_current <- s %/% 10 # check which split the athlete is in
-      if (split_current[j] != split[j]) { # if the current split exceeds the 
-      # former split, former split should be the current split, and the 
-      # acceleration rate should be changed too
+      
+      # check which split the athlete is in
+      split_current <- s %/% 10 
+      
+      # if the current split exceeds the former split, former split 
+      # should be the current split, and the acceleration rate 
+      # should be changed too
+      if (split_current[j] != split[j]) { 
         split[j] <- split_current[j] 
         s_exceeded <- s[j] %% 10
         t[j] <- splits[j, split[j] + 3][[1]] - splits[j, split[j] + 2][[1]]
